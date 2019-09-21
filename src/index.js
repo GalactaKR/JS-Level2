b45e624a7cf47d9517cd1df3c74df735cbf8ca66
TZL <903631647@qq.com>
2019-09-21 20:43:07 +0800

    add if and loop

--- src/index.js
+++ src/index.js
@@ -5,3 +5,24 @@ var addNumbers = function(x,y) {
 
 var testAddNumbers = addNumbers(5,6)
 console.log(testAddNumbers)
+
+// if (true) {
+//     console.log('it is true')
+
+// } else{
+//     console.log('it is false')
+// }
+
+if(testAddNumbers > 20) {
+    console.log('it is true') 
+} else{
+    console.log('it is false')
+
+}
+
+///////////////////////////loop///////////////////
+
+for (ii=0; ii < 100; ii = ii + 1 ){
+    var current = ii
+    console.log(current)
+}
